34c8261cb36cab1566d4d6b2b59adee46c0846ea
Simon Duerr <dev@simonduerr.eu>
2022-12-23 17:18:49 +0000

    fix: add github_action_utils

--- utils/check-markdownfiles.py
+++ utils/check-markdownfiles.py
@@ -5,12 +5,23 @@ import glob
 import sys
 
 from frontmatter import Frontmatter
+import github_action_utils as gha_utils
 
-files = glob.glob("content/notebooks/*.md")
+files = glob.glob("../content/notebooks/*.md")
 
-for f in files:
-    try:
-        post = Frontmatter.read_file(f)
-    except:
-        print(f"::error file={f} line=1::frontmatter could not be processed, please fix it")
-        sys.exit(1)
+gha_utils.debug(f"{files}")
+
+with gha_utils.group("My Group"):
+    for f in files:
+        try:
+            post = Frontmatter.read_file(f)
+        except:
+            gha_utils.error(
+                "fix frontmatter", title="Error processing Markdown", file=f,
+                col=1, end_column=2, line=1, end_line=2,
+            )
+
+gha_utils.error(
+                "test error", title="Error processing Markdown", file=f,
+                col=1, end_column=2, line=1, end_line=2,
+            )
