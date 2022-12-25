0fdcb0a5e1975fa24d3e98fd27bfc3669734a819
duerrsimon <duerrsimon@users.noreply.github.com>
2022-12-25 10:20:08 +0000

    deploy: 59e3ef60918fc543f9fe5d49a6793f056a8a88ed

--- _nuxt/ProseCodeInline.2444ef5b.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper.a1a6add7.js";import{o as r,J as n,R as t}from"./entry.d54f60b3.js";const s={};function c(e,a){return r(),n("code",null,[t(e.$slots,"default")])}const _=o(s,[["render",c]]);export{_ as default};
