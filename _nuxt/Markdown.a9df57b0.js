fa2876c02173e5862bc986685caab2cf4b454bba
duerrsimon <duerrsimon@users.noreply.github.com>
2022-12-26 09:35:39 +0000

    deploy: ce4c6830cbee5da1f8eab7824edba132183b21d2

---
+++ _nuxt/Markdown.a9df57b0.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.7b205769.js";import{f as o,h as f,k as u,y as m}from"./entry.626ba323.js";import"./utils.51fb7273.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=m(),{between:n,default:a}=f(),r=u(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
---
+++ _nuxt/ProseThead.8cfcf200.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.a1a6add7.js";import{o as r,J as t,R as s}from"./entry.626ba323.js";const a={};function n(e,c){return r(),t("thead",null,[s(e.$slots,"default")])}const _=o(a,[["render",n]]);export{_ as default};
