ba3109f57ba1b3b4bf941fe1301f704b934b3948
duerrsimon <duerrsimon@users.noreply.github.com>
2023-01-23 22:01:51 +0000

    deploy: 337a1316f6184c2815d9c51140ec0e9bbb991f23

---
+++ _nuxt/ProseTbody.8eec5f23.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.a1a6add7.js";import{o as r,J as t,R as s}from"./entry.10a393e3.js";const n={};function c(e,a){return r(),t("tbody",null,[s(e.$slots,"default")])}const _=o(n,[["render",c]]);export{_ as default};
--- submit/_payload.js
+++ submit/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1674509949871}
+export default {data:{},prerenderedAt:1674511310916}
