c3dc721e7cd61edaa02746a6532fdd7755246840
Simon Duerr <dev@simonduerr.eu>
2023-01-23 18:05:19 +0100

    fix: debug get_n_comments

--- scripts/src/colabdbmeta/fetch_github.py
+++ scripts/src/colabdbmeta/fetch_github.py
@@ -22,6 +22,7 @@ def get_n_comments(id: str):
         repo = gh.get_repo("colab-db/colab-db.github.io")
         all_issues = list(repo.get_issues(labels=["Comment"]))
         all_issues_titles = [issue.title for issue in all_issues]
+        print(f"fetching comments for /notebooks/{id}/ in", all_issues_titles)
         index =  all_issues_titles.index(f"/notebooks/{id}/")
         return  repo.get_issue(number=all_issues[index].number).comments
     except Exception as e:
