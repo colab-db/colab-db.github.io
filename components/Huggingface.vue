50dbd8a81d7123546e7e1160e1b34a8b0d8cfc13
Simon Dürr <dev@simonduerr.eu>
2023-02-18 19:53:50 +0100

    fix: author names hf component

--- components/Huggingface.vue
+++ components/Huggingface.vue
@@ -1,24 +1,24 @@
 <template>
     <article class="
-          lg:pt-20
-          pt-10
-          relative
-          flex
-          items-start
-          lg:space-x-10
-          px-[5%]
-          w-full
-        ">
+              lg:pt-20
+              pt-10
+              relative
+              flex
+              items-start
+              lg:space-x-10
+              px-[5%]
+              w-full
+            ">
         <div class="w-1/3">
             <ClientOnly>
                 <ContentRenderer class="
-                prose
-                lg:prose-base
-                prose-sm prose-slate
-                blog-link
-                pr-7
-                w-full
-              " :value="props.post">
+                    prose
+                    lg:prose-base
+                    prose-sm prose-slate
+                    blog-link
+                    pr-7
+                    w-full
+                  " :value="props.post">
                     <template #empty>
                         <p>No content found.</p>
                     </template>
@@ -40,18 +40,24 @@
                     <template v-for="(creator, k) in props.post.creator" :key="`creator-${k}`">
                         <div class="flex space-x-2 items-center">
                             <a class="
-                      text-base text-gray-600
-                      hover:text-primary-900
-                      underline underline-offset-4
-                      decoration-wavy decoration-primary/40
-                      hover:decoration-primary
-                      transition-all
-                    ">
+                          text-base text-gray-600
+                          hover:text-primary-900
+                          underline underline-offset-4
+                          decoration-wavy decoration-primary/40
+                          hover:decoration-primary
+                          transition-all
+                        ">
                                 {{ creator.name }}
                             </a>
-                            <Icon icon="ant-design:github-filled" class="text-gray-500 w-5 h-5" />
-                            <Icon icon="ant-design:twitter" class="text-blue-500 w-5 h-5" />
-                            <Icon icon="academicons:orcid" class="text-green-500 w-5 h-5" />
+                            <NuxtLink :to="'https://github.com/' + creator.github" v-if="checkIfEmpty(creator.github)">
+                                <Icon icon="ant-design:github-filled" class="text-gray-500 w-5 h-5" />
+                            </NuxtLink>
+                            <NuxtLink :to="'https://twitter.com/' + creator.twitter" v-if="checkIfEmpty(creator.twitter)">
+                                <Icon icon="ant-design:twitter" class="text-blue-500 w-5 h-5" />
+                            </NuxtLink>
+                            <NuxtLink :to="'https://orcid.org/' + creator.orcid" v-if="checkIfEmpty(creator.orcid)">
+                                <Icon icon="academicons:orcid" class="text-green-500 w-5 h-5" />
+                            </NuxtLink>
                         </div>
                     </template>
                 </div>
@@ -60,16 +66,16 @@
                 <div class="flex items-center space-x-2">
                     <template v-for="(t, k) in props.post.tags" :key="`creator-${k}`">
                         <span class="
-                    bg-blue-100
-                    text-blue-800 text-sm
-                    font-medium
-                    px-2.5
-                    py-0.5
-                    m-1
-                    rounded
-                    dark:bg-blue-200 dark:text-blue-800
-                    whitespace-nowrap
-                  ">{{ t }}</span>
+                        bg-blue-100
+                        text-blue-800 text-sm
+                        font-medium
+                        px-2.5
+                        py-0.5
+                        m-1
+                        rounded
+                        dark:bg-blue-200 dark:text-blue-800
+                        whitespace-nowrap
+                      ">{{ t }}</span>
                     </template>
                 </div>
 
@@ -77,16 +83,16 @@
                 <div class="flex items-center space-x-2">
                     <template v-for="(software, k) in props.post.used_software" :key="`software-${k}`">
                         <span class="
-                    bg-blue-100
-                    text-blue-800 text-sm
-                    font-medium
-                    px-2.5
-                    py-0.5
-                    m-1
-                    rounded
-                    dark:bg-blue-200 dark:text-blue-800
-                    whitespace-nowrap
-                  ">
+                        bg-blue-100
+                        text-blue-800 text-sm
+                        font-medium
+                        px-2.5
+                        py-0.5
+                        m-1
+                        rounded
+                        dark:bg-blue-200 dark:text-blue-800
+                        whitespace-nowrap
+                      ">
                             {{ software }}</span>
                     </template>
                 </div>
@@ -99,10 +105,10 @@
         <div class="w-2/3 border-2 rounded-xl">
             <iframe :src="'https://hf.space/embed/' + props.post.git + '/'" frameBorder="0" title="Gradio app"
                 class="container p-0 h-screen flex-grow space-iframe" allow="accelerometer; ambient-light-sensor; autoplay; battery; camera; document-domain; encrypted-media;
-            fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi;
-            oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; vr ; wake-lock;
-            xr-spatial-tracking" sandbox="allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-same-origin
-            allow-scripts allow-downloads"></iframe>
+                fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi;
+                oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; vr ; wake-lock;
+                xr-spatial-tracking" sandbox="allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-same-origin
+                allow-scripts allow-downloads"></iframe>
 
 
         </div>
@@ -113,4 +119,15 @@
 import { Icon } from "@iconify/vue";
 import GithubButton from "vue-github-button";
 const props = defineProps(['post'])
+
+function checkIfEmpty(val) {
+    if (val == undefined || val == null) {
+        return false
+    } else if (val.length <= 1) {
+        return false
+    } else {
+        return true
+    }
+}
+
 </script>
